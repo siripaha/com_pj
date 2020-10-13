4c9dbef1930b5f38ff0f1b5347fee14a39f45ec8
muthita munmueang <muthita.mu.61@ubu.ac.th>
2020-10-13 23:42:00 +0700

    final

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import routes from "./routes";
+import routes from "./route";
 import { useRoutes } from "react-router-dom";
 
 function App() {
