6e9aaa9d2f27a4f7cbbfb02cc42ab6b0e97bb86a
siripaha <siripapha.ch.61@ubu.ac.th>
2020-10-28 23:21:42 +0700

    Updates

---
+++ precache-manifest.aeaf30a2d398d81b973ee333026032ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e27655c208046f32578fda15dfb7739d",
+    "url": "/com_pj/index.html"
+  },
+  {
+    "revision": "347c9171b143a4f7e2eb",
+    "url": "/com_pj/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ba94592ae05bbb12a905",
+    "url": "/com_pj/static/js/2.e6162beb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/com_pj/static/js/2.e6162beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347c9171b143a4f7e2eb",
+    "url": "/com_pj/static/js/main.d6fad5bf.chunk.js"
+  },
+  {
+    "revision": "1f8acb4e12ca1b609981",
+    "url": "/com_pj/static/js/runtime-main.134a61bf.js"
+  }
+]);
